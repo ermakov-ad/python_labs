ac0a320277ea694d69a21a170fce98704b94842e
ermakov-ad <74365049+ermakov-ad@users.noreply.github.com>
2021-12-13 03:51:59 +0500

    Add files via upload
    change solution 2 episode

--- ep_2.py
+++ ep_2.py
@@ -1,14 +1,16 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 
 path = "flights.csv"
 
 data = pd.read_csv(path, sep=',')
 
+data_count = data.groupby('CARGO').count()
 data_sum = data.groupby('CARGO').sum()
+data_sum.iloc[:, 0] = data_count.iloc[:, 0]
 
-data_sum.iloc[:, 0] = data_sum.iloc[:, 0].apply(lambda x: ((x*8.0 + 1)**0.5)/2.0 - 0.5)
+#data_sum.iloc[:, 0] = data_sum.iloc[:, 0].apply(lambda x: ((x*8.0 + 1)**0.5)/2.0 + 0.5) # (sum(0, 1, 2, ... n))^(-1), f(sum) = n
 print(data_sum)
 
 bar_numbers = range(len(data_sum))
